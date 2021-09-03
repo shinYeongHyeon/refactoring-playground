b159e06e49b8f7ba31459d1a211caf25e824b0c8
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-09-04 02:22:38 +0900

    Function Name

--- main.ts
+++ main.ts
@@ -53,29 +53,40 @@ function statement(invoice: any, plays: any) {
         return result;
     }
 
+    function volumeCreditsFor(aPerformance: any) {
+        let result = 0;
+
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+
+        return result;
+    }
+
+    function usd(aNumber: number) {
+        return new Intl
+          .NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        })
+          .format(aNumber/100);
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = 0;
-
-        thisAmount = amountFor(perf);
-
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
 
     return result;
