87d081a993cd988f845d9df3661da37033331ae3
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-09-04 02:10:10 +0900

    Rename Parameter

--- main.ts
+++ main.ts
@@ -54,22 +54,22 @@ function statement(invoice: any, plays: any) {
 }
 
 
-function amountFor(perf: any, play: any) {
+function amountFor(aPerformance: any, play: any) {
     let result = 0;
 
     switch (play.type) {
         case "tragedy": // 비극
             result = 40000;
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy": // 희극
             result = 30000;
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르 : ${play.type}`);
