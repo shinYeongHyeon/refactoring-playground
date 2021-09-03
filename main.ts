450a2bad00d1b1343743538547392c208505b7c3
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-09-04 02:26:44 +0900

    누적 값 지우기

--- main.ts
+++ main.ts
@@ -74,20 +74,32 @@ function statement(invoice: any, plays: any) {
           .format(aNumber/100);
     }
 
-    let totalAmount = 0;
-    let volumeCredits = 0;
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+
+        return result;
+    }
+
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
-        // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
     return result;
 }
